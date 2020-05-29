908261763453d7e3d9917a412d5d6c6b46b0d309
geoff <geoff.newman@immersive.video>
2020-05-29 16:02:28 +1000

    Initial commit

---
+++ Assets/cameraTextImage/scripts/TextToImage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextToImage : MonoBehaviour
+{
+    public Sprite finalSprite;
+    public Texture2D texture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        finalSprite.texture = texture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
